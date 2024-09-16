0053e3ae1c6e3eaf5b3bb167b47164dbcadb92ca
Kunal verma <kunal33grd@gmail.com>
2024-09-16 16:11:56 +0530

    modified js and deleted few songs

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function secTomint(seconds) {
 async function getSongs(folder) {
     currFolder = folder;
 
-    let a = await fetch(`http://127.0.0.1:3000/${folder}/`);
+    let a = await fetch(`/${folder}/`);
     let response = await a.text();
     // console.log(response)
 
@@ -81,7 +81,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:3000/songs/`);
+    let a = await fetch(`/songs/`);
     let response = await a.text();
 
     let div = document.createElement("div")
@@ -98,7 +98,7 @@ async function displayAlbums() {
             let folder = e.href.split("/").slice(-2)[0]
 
             //get metadata of the folder
-            let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
+            let a = await fetch(`/songs/${folder}/info.json`)
             let response = await a.json();
 
             cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}" class="card">
